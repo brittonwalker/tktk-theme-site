bcaec4190b058bcbc7a5ea0c587b8cee7f1f6915
Britton Walker <bwalker1801@gmail.com>
2022-01-29 16:51:01 -0500

    Fix prism, import accordion

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,10 @@
-import 'prismjs';
+import Prism from 'prismjs';
+import 'prismjs/components/prism-markup-templating';
 import 'prismjs/components/prism-css';
 import 'prismjs/components/prism-css-extras';
 import 'prismjs/components/prism-scss';
-import 'prismjs/components/prism-markup-templating';
 import 'prismjs/components/prism-twig';
+Prism.highlightAll();
 
 import './app/Header';
 import './app/Slideshows';
@@ -13,3 +14,4 @@ import './app/CodeTabs';
 import './app/ProgressBar';
 import './app/Drawer';
 import './app/Toast';
+import './app/Accordion';
